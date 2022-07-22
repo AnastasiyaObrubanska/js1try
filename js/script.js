f2634a95c9c735b3d68490db031f586ae9046ce3
Anastasiya <happyinception@gmail.com>
2022-07-22 14:04:33 +0300

    adding 1st lesson of application .js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log(1);
+
+const numberOfFilms = +prompt('Скільки фільмів ви вже подивилися?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один з останніх переглянутих фільмів?', ''),
+      b = prompt('На скільки ви його оціните', ''),
+      c = prompt('Один з останніх переглянутих фільмів?', ''),
+      d = prompt('На скільки ви його оціните', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
